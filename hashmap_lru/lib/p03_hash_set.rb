7da30770c000c474d0cf3ac8b3846f4f35105a69
Aaron <astrauli@gmail.com>
2017-12-30 13:08:46 -0800

    hash_set implemented

--- hashmap_lru/lib/p03_hash_set.rb
+++ hashmap_lru/lib/p03_hash_set.rb
@@ -9,18 +9,29 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+      num = key.hash
+      self[num] << key
+      @count += 1
+    end
+    resize! if num_buckets < @count
   end
 
   def include?(key)
+    num = key.hash
+    self[num].include?(key)
   end
 
   def remove(key)
+    num = key.hash
+    self[num].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +39,9 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each { |num| insert(num) }
   end
 end
