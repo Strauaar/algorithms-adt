e61bf2365c1cc906361ea5c029e9d6d717ac7938
Aaron <astrauli@gmail.com>
2018-01-08 21:58:30 -0800

    add graphs and topologicaal sorts files

--- aaron_strauli_bst/lib/binary_search_tree.rb
+++ aaron_strauli_bst/lib/binary_search_tree.rb
@@ -1,34 +1,86 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
-
+require_relative 'bst_node'
 class BinarySearchTree
+  attr_accessor :root
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if @root.nil?
+      @root = BSTNode.new(value)
+    else
+      node = @root
+      until node.nil?
+        current_position = nil
+        if value <= node.value
+          node.left.nil? ? node.left = BSTNode.new(value) : current_position = node.left
+        else
+          node.right.nil? ? node.right = BSTNode.new(value) : current_position = node.right
+        end
+        node = current_position
+      end
+    end
   end
 
   def find(value, tree_node = @root)
+    return nil if tree_node.nil?
+    return tree_node if value == tree_node.value
+    return find(value, tree_node.left) if value < tree_node.value
+    return find(value, tree_node.right) if value > tree_node.value
+  end
+
+  def maximum(tree_node = @root)
+    node = tree_node
+    while node.right
+      node = node.right
+    end
+    node
   end
 
   def delete(value)
+    node = find(value)
+    return nil if node.nil?
+    if node.left == nil && node.right == nil
+      if node == @root
+        @root = nil
+        return nil
+      end
+      node.parent.remove_child(value)
+    elsif (node.right == nil && node.left != nil) || (node.right != nil && node.left == nil)
+      child = node.right == nil ? node.left : node.right
+      node.parent.replace_child(value, child)
+    else
+      replacement = maximum(node.left)
+      replacement.parent.replace_child(replacement.value, replacement.left)
+      replacement.left = node.left
+      replacement.right = node.right
+      node.parent.replace_child(value, replacement)
+    end
   end
 
-  # helper method for #delete:
-  def maximum(tree_node = @root)
-  end
 
-  def depth(tree_node = @root)
-  end 
+  def depth
+
+  end
 
   def is_balanced?(tree_node = @root)
+
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    if tree_node
+      traverse_left = in_order_traversal(tree_node.left, arr)
+      arr << tree_node.value
+      traverse_right = in_order_traversal(tree_node.right, arr)
+    end
+    arr
   end
 
 
   private
   # optional helper methods go here:
+
 
 end
--- aaron_strauli_bst/lib/bst_node.rb
+++ aaron_strauli_bst/lib/bst_node.rb
@@ -1,4 +1,36 @@
 class BSTNode
+  attr_reader :value
+  attr_accessor :left, :right, :parent
   def initialize(value)
+    @value = value
   end
+
+  def left=(left_node)
+    left_node.parent = self
+    @left = left_node
+  end
+
+  def right=(right_node)
+    right_node.parent = self
+    @right = right_node
+  end
+
+  def remove_child(value)
+   if value == @left.value
+     @left = nil
+   elsif value == @right.value
+     @right = nil
+   else
+     return nil
+   end
+  end
+
+  def replace_child(child_value, node)
+    if child_value == @left.value
+      @left = node
+    elsif child_value == @right.value
+      @right = node
+    end
+  end
+
 end
